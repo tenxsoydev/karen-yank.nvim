da81b4b0284dee087a1d4d35d4be93ff281ce4da
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-11-11 14:02:34 +0100

    fix collision with tpope's surround
    making the autocommand a little lazy seems to solve it

--- lua/karen-yank/autocmds.lua
+++ lua/karen-yank/autocmds.lua
@@ -20,10 +20,15 @@ function M.set_aus(num_reg_opts)
 	vim.api.nvim_create_autocmd("TextYankPost", {
 		pattern = { "*", "+" },
 		callback = function()
-			if vim.fn.getreg(0) ~= vim.fn.getreg "+" then handlers.sync_regs(0, "+") end
-
-			if not num_reg_opts.deduplicate or not num_reg_opts.deduplicate.enable then return end
-			handlers.handle_duplicates(num_reg_opts.transitory_reg, num_reg_opts.deduplicate.no_whitespace)
+			vim.loop.new_timer():start(
+				50,
+				0,
+				vim.schedule_wrap(function()
+					if vim.fn.getreg(0) ~= vim.fn.getreg "+" then handlers.sync_regs(0, "+") end
+					if not num_reg_opts.deduplicate or not num_reg_opts.deduplicate.enable then return end
+					handlers.handle_duplicates(num_reg_opts.transitory_reg, num_reg_opts.deduplicate.no_whitespace)
+				end)
+			)
 		end,
 	})
 end
