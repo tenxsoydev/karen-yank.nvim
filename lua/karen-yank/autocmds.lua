e81e771b92cd751392afc08b0e3227f0209a80d2
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-11-11 04:03:08 +0100

    abstract load function to variable

--- lua/karen-yank/autocmds.lua
+++ lua/karen-yank/autocmds.lua
@@ -2,12 +2,12 @@ local M = {}
 
 ---@param num_reg_opts NumberRegOpts
 function M.set_aus(num_reg_opts)
+	local handlers = require "karen-yank.handlers"
+
 	if vim.o.clipboard ~= "unnamedplus" then
 		vim.api.nvim_create_autocmd("TextYankPost", {
 			callback = function()
-				if num_reg_opts.deduplicate then
-					require("karen-yank.handlers").handle_duplicates(num_reg_opts.transitory_reg)
-				end
+				if num_reg_opts.deduplicate then handlers.handle_duplicates(num_reg_opts.transitory_reg) end
 			end,
 		})
 
@@ -18,10 +18,8 @@ function M.set_aus(num_reg_opts)
 		vim.api.nvim_create_autocmd("TextYankPost", {
 			pattern = { "*", "+" },
 			callback = function()
-				require("karen-yank.handlers").sync_regs(0, "+")
-				if num_reg_opts.deduplicate then
-					require("karen-yank.handlers").handle_duplicates(num_reg_opts.transitory_reg)
-				end
+				handlers.sync_regs(0, "+")
+				if num_reg_opts.deduplicate then handlers.handle_duplicates(num_reg_opts.transitory_reg) end
 			end,
 		})
 	end
