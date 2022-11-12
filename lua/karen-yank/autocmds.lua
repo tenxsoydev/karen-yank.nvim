0678a800d09a0260aee1ce66aa10f55466f03c1d
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-11-12 15:35:52 +0100

    fix cuts in visual mode

--- lua/karen-yank/autocmds.lua
+++ lua/karen-yank/autocmds.lua
@@ -6,14 +6,17 @@ local handlers = require "karen-yank.handlers"
 function M.set_aus(num_reg_opts)
 	vim.api.nvim_create_autocmd("TextYankPost", {
 		callback = function()
-			-- for now, keep deprecated 'num_reg_opts.deduplicate' setting, which was a simple bool before
-			if not num_reg_opts.deduplicate
-				 or not num_reg_opts.deduplicate.enable
-				 or not (vim.v.register:match "%d+" or vim.v.register == "+" or vim.v.register == '"')
+			if
+				not num_reg_opts.deduplicate
+				or not num_reg_opts.deduplicate.enable
+				or not (vim.v.register:match "%d+" or vim.v.register == "+" or vim.v.register == '"')
 			then
 				return
 			end
 			handlers.handle_duplicates(num_reg_opts.transitory_reg, num_reg_opts.deduplicate.no_whitespace)
+
+			if vim.o.clipboard == "" then return end
+			if vim.fn.getreg "+" ~= vim.fn.getreg "0" then handlers.sync_regs("+", "0") end
 		end,
 	})
 end
