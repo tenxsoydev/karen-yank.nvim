4f44125e199fbff70ce271653422fad7a52c4f1d
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-11-12 22:13:40 +0100

    fix paste in visual cond. (regression 5b1a93d)

--- lua/karen-yank/autocmds.lua
+++ lua/karen-yank/autocmds.lua
@@ -15,7 +15,13 @@ function M.set_aus(num_reg_opts)
 				return
 			end
 
-			if vim.o.clipboard ~= "" and vim.fn.getreg "+" ~= vim.fn.getreg "0" then handlers.sync_regs("+", "0") end
+			if
+				vim.o.clipboard ~= ""
+				and vim.fn.getreg "+" ~= vim.fn.getreg "0"
+				and vim.fn.getreg "-" ~= vim.fn.getreg '"'
+			then
+				handlers.sync_regs("+", "0")
+			end
 
 			-- remove duplicate 0,1 in case of doulbe `ydd` the same line
 			if vim.fn.getreg "1" == vim.fn.getreg "0" then
