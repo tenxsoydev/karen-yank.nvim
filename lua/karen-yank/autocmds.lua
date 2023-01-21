26917896764d5d20167d4c0ffcde5bcccb4b620a
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-01-21 17:07:14 +0100

    defer autocmd fix? #13

--- lua/karen-yank/autocmds.lua
+++ lua/karen-yank/autocmds.lua
@@ -8,23 +8,25 @@ function M.set_aus(num_reg_opts)
 
 	vim.api.nvim_create_autocmd("TextYankPost", {
 		callback = function()
-			-- use only relevant registers
-			if not (vim.v.register:match "%d+" or vim.v.register == "+" or vim.v.register == '"') then return end
+			vim.defer_fn(function()
+				-- use only relevant registers
+				if not (vim.v.register:match "%d+" or vim.v.register == "+" or vim.v.register == '"') then return end
 
-			if vim.fn.getreg "+" ~= vim.fn.getreg '"' then handlers.sync_regs("+", '"') end
+				if vim.fn.getreg "+" ~= vim.fn.getreg '"' then handlers.sync_regs("+", '"') end
 
-			-- remove duplicate 0,1 in case of doulbe `ydd` the same line
-			if vim.fn.getreg "1" == vim.fn.getreg "0" then
-				for x = 0, 8 do
-					handlers.sync_regs(x, x + 1)
+				-- remove duplicate 0,1 in case of doulbe `ydd` the same line
+				if vim.fn.getreg "1" == vim.fn.getreg "0" then
+					for x = 0, 8 do
+						handlers.sync_regs(x, x + 1)
+					end
 				end
-			end
 
-			handlers.handle_duplicates(num_reg_opts.transitory_reg, num_reg_opts.deduplicate.ignore_whitespace)
-			vim.defer_fn(
-				function() vim.fn.setreg(num_reg_opts.transitory_reg.reg, num_reg_opts.transitory_reg.placeholder) end,
-				20
-			)
+				handlers.handle_duplicates(num_reg_opts.transitory_reg, num_reg_opts.deduplicate.ignore_whitespace)
+				vim.defer_fn(
+					function() vim.fn.setreg(num_reg_opts.transitory_reg.reg, num_reg_opts.transitory_reg.placeholder) end,
+					20
+				)
+			end, 50)
 		end,
 	})
 end
